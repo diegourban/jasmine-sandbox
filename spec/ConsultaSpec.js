f9e34727ff61d7ff2dc4211001e501e15d49b86f
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-19 21:51:23 -0200

    adding some more tests

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -17,4 +17,11 @@ describe("Consulta", function() {
 		var consulta = new Consulta(guilherme, ["procedimento-comum", "raio-x", "procedimento-comum", "gesso"], false , false);
 		expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
 	});
+
+	 it("should double the price on a private appointment", function() {
+        var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
+        var consulta = new Consulta(guilherme, ["proc1", "proc2"], true, false);
+
+        expect(consulta.preco()).toEqual(50 * 2);
+    });
 });
--- spec/PacienteSpec.js
+++ spec/PacienteSpec.js
@@ -5,4 +5,10 @@ describe("Paciente", function() {
 
 		expect(guilherme.imc()).toEqual(72 / (1.82 * 1.82));
 	});
+
+	it("should calculate heart beatings", function() {
+		var guilherme = new Paciente("Guilherme", 28, 72, 1.82);
+
+		expect(guilherme.batimentos()).toEqual(28 * 365 * 24 * 60 * 80);
+	});
 });
