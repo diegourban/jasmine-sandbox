e8f586feada9689e7bc37cedca2bdf4c88b19f50
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-20 12:19:55 -0200

    Update ConsultaSpec.js

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -1,27 +1,37 @@
 describe("Consulta", function() {
 
-	it("should not charge if is return", function() {
-		var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
-		var consulta = new Consulta(guilherme, [], true , true);
-		expect(consulta.preco()).toEqual(0);
+	var guilherme;
+
+ 	// before each test
+	beforeEach(function() {
+		//guilherme = new Paciente("Guilherme", 28, 72, 1.80); 
+		guilherme = new PacienteBuilder().constroi();
 	});
 
-	it("should charge 25 for every common procedure", function() {
-		var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
-		var consulta = new Consulta(guilherme, ["proc1", "proc2"], false , false);
-		expect(consulta.preco()).toEqual(50);
+	// grouping tests
+	describe("returning appointment", function() {
+		it("should not charge if is return", function() {
+			var consulta = new Consulta(guilherme, [], true , true);
+			expect(consulta.preco()).toEqual(0);
+		});
+
+		it("should double the price on a private appointment", function() {
+	        var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
+	        var consulta = new Consulta(guilherme, ["proc1", "proc2"], true, false);
+
+	        expect(consulta.preco()).toEqual(50 * 2);
+	    });
 	});
 
-	it("should charge specific price depending of the procedure", function() {
-		var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
-		var consulta = new Consulta(guilherme, ["procedimento-comum", "raio-x", "procedimento-comum", "gesso"], false , false);
-		expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+	describe("appointment with procedures", function() {
+		it("should charge 25 for every common procedure", function() {
+			var consulta = new Consulta(guilherme, ["proc1", "proc2"], false , false);
+			expect(consulta.preco()).toEqual(50);
+		});
+
+		it("should charge specific price depending of the procedure", function() {
+			var consulta = new Consulta(guilherme, ["procedimento-comum", "raio-x", "procedimento-comum", "gesso"], false , false);
+			expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+		});
 	});
-
-	 it("should double the price on a private appointment", function() {
-        var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
-        var consulta = new Consulta(guilherme, ["proc1", "proc2"], true, false);
-
-        expect(consulta.preco()).toEqual(50 * 2);
-    });
 });
